86c60ff380567f4bc7c37ed224caf78cc06a4146
zhangsiqi <1098795322@qq.com>
2017-10-26 14:08:11 +0800

    添加MD5测试单元测试

---
+++ src/test/java/com/mmall/test/Md5test.java
@@ -0,0 +1,13 @@
+package com.mmall.test;
+
+import com.mmall.util.MD5Util;
+import org.junit.Test;
+
+public class Md5test {
+
+    @Test
+    public  void md5SaltTest(){
+        System.out.println(MD5Util.MD5EncodeUtf8("admin"));
+    }
+
+}
