b16b62c0d946c41ed202d8a85172d02590379c25
ZhangSiQi <1098795322@qq.com>
2020-04-01 17:32:37 +0800

    将远程ftp服务器修改到本地，改变配置文件。

--- src/main/java/com/mmall/service/impl/ProductServiceImpl.java
+++ src/main/java/com/mmall/service/impl/ProductServiceImpl.java
@@ -157,7 +157,7 @@ public class ProductServiceImpl implements IProductService {
         productListVo.setId(product.getId());
         productListVo.setName(product.getName());
         productListVo.setCategoryId(product.getCategoryId());
-        productListVo.setImageHost(PropertiesUtil.getProperty("ftp.server.http.prefix", "http://img.cookcode.com.cn/"));
+        productListVo.setImageHost(PropertiesUtil.getProperty("ftp.server.http.prefix"));
         productListVo.setMainImage(product.getMainImage());
         productListVo.setPrice(product.getPrice());
         productListVo.setSubtitle(product.getSubtitle());
